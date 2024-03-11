2b713b486c610092fd39b24ca482fcc5f018bf5b
VNMod <varunnmodak@vt.edu>
2024-03-10 21:44:26 -0400

    better recording workflow

--- f1tenth_ws/src/f1tenth_system/camera/camera/test_subscriber.py
+++ f1tenth_ws/src/f1tenth_system/camera/camera/test_subscriber.py
@@ -15,6 +15,7 @@ class test_subscriber(Node):
         self.image_count = 0
         self.output_folder = 'dataset/score1'
         self.output_folder_path = os.path.abspath(self.output_folder)
+        self.save_flag = False
         if not os.path.isdir(self.output_folder_path):
             os.makedirs(self.output_folder_path)  # make sure the directory exists
 
@@ -24,6 +25,23 @@ class test_subscriber(Node):
         self.get_logger().warning("Receiving RGB frame")
         current_frame = self.bridge.imgmsg_to_cv2(data)
         
+        if self.save_flag:
+            # save the frame to the local disk:
+            image_path = os.path.join(self.output_folder_path, f'image_{self.image_count:04d}.jpg')
+            cv2.imwrite(image_path, current_frame)
+            self.get_logger().info(f'Saved image: {image_path}')
+            self.image_count += 1
+            self.save_flag = False
+        
+        # to show each frame:
+        cv2.imshow("RGB image", current_frame)
+        key = cv2.waitKey(1)  # Wait 1ms for a key press
+
+        if key == ord('s'):  # Set the flag to True if the key pressed is 's'
+            self.save_flag = True
+        elif key == ord('q'):  # Quit if the key pressed is 'q'
+            cv2.destroyAllWindows()
+
         # # to show each frame:
         # cv2.imshow("RGB image", current_frame)
         # cv2.waitKey(1)
@@ -34,23 +52,21 @@ class test_subscriber(Node):
         # self.get_logger().info(f'Saved image: {image_path}')
         # self.image_count += 1
 
-        # to show each frame:
-        cv2.imshow("RGB image", current_frame)
-        key = cv2.waitKey(0)  # Wait indefinitely for a key press
+        # # to show each frame:
+        # cv2.imshow("RGB image", current_frame)
+        # key = cv2.waitKey(0)  # Wait indefinitely for a key press
 
-        if key == ord('s'):  # Save the image if the key pressed is 's'
-            # save the frame to the local disk:
-            image_path = os.path.join(self.output_folder_path, f'image_{self.image_count:04d}.jpg')
-            cv2.imwrite(image_path, current_frame)
-            self.get_logger().info(f'Saved image: {image_path}')
-            self.image_count += 1
-        elif key == ord('q'):  # Quit if the key pressed is 'q'
-            cv2.destroyAllWindows()
-
+        # if key == ord('s'):  # Save the image if the key pressed is 's'
+        #     # save the frame to the local disk:
+        #     image_path = os.path.join(self.output_folder_path, f'image_{self.image_count:04d}.jpg')
+        #     cv2.imwrite(image_path, current_frame)
+        #     self.get_logger().info(f'Saved image: {image_path}')
+        #     self.image_count += 1
+        # elif key == ord('q'):  # Quit if the key pressed is 'q'
+        #     cv2.destroyAllWindows()
 
         
-        
-        
+                
 def main(args=None):
     rclpy.init(args=args)
     node = test_subscriber()
