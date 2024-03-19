d1c9ce8bf1d5f728bc207ba6ffe91a50085b58c6
Dong Li <dongli@vt.edu>
2024-03-18 22:02:46 -0400

    adding pics for score6

--- f1tenth_ws/build/vesc/colcon_command_prefix_build.sh
+++
@@ -1,4 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/nx-ros2/varun_ws/f1tenth_ws/install/vesc_msgs/share/vesc_msgs/package.sh"
-. "/home/nx-ros2/varun_ws/f1tenth_ws/install/vesc_ackermann/share/vesc_ackermann/package.sh"
-. "/home/nx-ros2/varun_ws/f1tenth_ws/install/vesc_driver/share/vesc_driver/package.sh"
