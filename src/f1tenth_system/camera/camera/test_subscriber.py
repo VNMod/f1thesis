56e8fe2a0a0492a424487a1f0fd47f535b524601
Dong Li <dongli@vt.edu>
2024-03-26 11:07:33 -0400

    adding more score1s

--- src/f1tenth_system/camera/camera/test_subscriber.py
+++ src/f1tenth_system/camera/camera/test_subscriber.py
@@ -30,7 +30,7 @@ class test_subscriber(Node):
         
         if self.save_flag:
             # save the frame to the local disk:
-            image_path = os.path.join(self.output_folder_path, f'image_{self.image_count:04d}.jpg')
+            image_path = os.path.join(self.output_folder_path, f'image_{self.image_count:05d}.jpg')
             cv2.imwrite(image_path, current_frame)
             self.get_logger().info(f'Saved image: {image_path}')
             self.image_count += 1
